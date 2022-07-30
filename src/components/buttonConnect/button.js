12d55a08916b68aea51d1df79a4709a68cb9003a
adriankorp <adrian.korpowski@gmail.com>
2022-07-30 11:20:01 +0200

    remove unnecessary  code

--- src/components/buttonConnect/button.js
+++ src/components/buttonConnect/button.js
@@ -1,94 +1,13 @@
-import { useEffect, useState } from "react";
-import {
-  VStack,
-  useDisclosure,
-  Button,
-  Text,
-  HStack,
-  Select,
-  Input,
-  Box,
-} from "@chakra-ui/react";
+import { VStack, useDisclosure, Button, HStack } from "@chakra-ui/react";
 import SelectWalletModal from "./buttonModal";
 import { useWeb3React } from "@web3-react/core";
-import { networkParams } from "../../networks";
-import { connectors } from "../../connectors";
-import { toHex, truncateAddress } from "../../utils";
 
 export default function ConnectWallet() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { library, chainId, account, activate, deactivate, active } =
-    useWeb3React();
-  const [signature, setSignature] = useState("");
-  const [error, setError] = useState("");
-  const [network, setNetwork] = useState(undefined);
-  const [message, setMessage] = useState("");
-  const [signedMessage, setSignedMessage] = useState("");
-  const [verified, setVerified] = useState();
-
-  const handleNetwork = (e) => {
-    const id = e.target.value;
-    setNetwork(Number(id));
-  };
-
-  const handleInput = (e) => {
-    const msg = e.target.value;
-    setMessage(msg);
-  };
-
-  const switchNetwork = async () => {
-    try {
-      await library.provider.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: toHex(network) }],
-      });
-    } catch (switchError) {
-      if (switchError.code === 4902) {
-        try {
-          await library.provider.request({
-            method: "wallet_addEthereumChain",
-            params: [networkParams[toHex(network)]],
-          });
-        } catch (error) {
-          setError(error);
-        }
-      }
-    }
-  };
-
-  const signMessage = async () => {
-    if (!library) return;
-    try {
-      const signature = await library.provider.request({
-        method: "personal_sign",
-        params: [message, account],
-      });
-      setSignedMessage(message);
-      setSignature(signature);
-    } catch (error) {
-      setError(error);
-    }
-  };
-
-  const verifyMessage = async () => {
-    if (!library) return;
-    try {
-      const verify = await library.provider.request({
-        method: "personal_ecRecover",
-        params: [signedMessage, signature],
-      });
-      setVerified(verify === account.toLowerCase());
-    } catch (error) {
-      setError(error);
-    }
-  };
+  const { deactivate, active } = useWeb3React();
 
   const refreshState = () => {
     window.localStorage.setItem("provider", undefined);
-    setNetwork("");
-    setMessage("");
-    setSignature("");
-    setVerified(undefined);
   };
 
   const disconnect = () => {
@@ -96,14 +15,9 @@ export default function ConnectWallet() {
     deactivate();
   };
 
-  useEffect(() => {
-    const provider = window.localStorage.getItem("provider");
-    if (provider) activate(connectors[provider]);
-  }, []);
-
   return (
     <>
-      <VStack >
+      <VStack>
         <HStack>
           {!active ? (
             <Button onClick={onOpen}>Connect Wallet</Button>
