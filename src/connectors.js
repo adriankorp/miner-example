ce4566e2568bed8c0a1b2c44421d98700ef0ea52
adriankorp <adrian.korpowski@gmail.com>
2022-07-30 11:14:11 +0200

    delate WalletLink import

--- src/connectors.js
+++ src/connectors.js
@@ -1,6 +1,6 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+
 
 const injected = new InjectedConnector({
   supportedChainIds: [5],
