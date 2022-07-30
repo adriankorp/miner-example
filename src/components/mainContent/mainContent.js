858d39e314317adbe8190377443f7d6976a93012
adriankorp <adrian.korpowski@gmail.com>
2022-07-30 11:18:17 +0200

    add functtion getting contract data

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -1,94 +1,155 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./mainContent.css";
-import {
-  VStack,
-  useDisclosure,
-  Button,
-  Text,
-  HStack,
-  Select,
-  Input,
-  Box,
-} from "@chakra-ui/react";
-import Rewards from "../rewards/rewards";
-
-const REFLINK = "0x8b41dc64c8eF2B01769097eBF8b85BAf1054f3F5";
-
+import { VStack, HStack, Box } from "@chakra-ui/react";
+import Rewards from "../rewards";
+import InfoBox from "../info";
+import Referal from "../referal";
+import MainSection from "../mainSection";
+import { useWeb3React } from "@web3-react/core";
+import { useMediaQuery } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import { contractAddress } from "../../utils";
+import { abi_miner } from "../../constans/contract";
 function MainContent() {
-  const [ethValue, setEthValue] = useState(0);
+  const [isSmallerThan950] = useMediaQuery("(max-width: 950px)");
+  const { account, library, active } = useWeb3React();
+  const [minerContract, setMinerContract] = useState();
+  const [contractBalance, setContractBalance] = useState(0);
+  const [accountBalance, setAccountBalance] = useState(0);
+  const [myEarns, setMyEarns] = useState(0);
+  const [myMiners, setMyMiners] = useState(0);
+
+  const getContract = async () => {
+    let minerContract = new ethers.Contract(
+      contractAddress,
+      abi_miner,
+      await library.getSigner()
+    );
+    console.log(minerContract);
+    setMinerContract(minerContract);
+  };
+
+  const getContractStats = async () => {
+    if (!library) return;
+    try {
+      console.log("Refresh");
+      let accountBalance = await library.getBalance(account);
+
+      let contractBalance = await minerContract.getBalance();
+      setAccountBalance(
+        parseFloat(ethers.utils.formatEther(accountBalance.toString())).toFixed(
+          3
+        )
+      );
+      setContractBalance(
+        parseFloat(
+          ethers.utils.formatEther(contractBalance.toString())
+        ).toFixed(3)
+      );
+      let miners = await minerContract.hatcheryMiners(account);
+      setMyMiners(miners.toString());
+      const myMinersSinceLastHatch = await minerContract.getMyMiners();
+
+      if (myMinersSinceLastHatch > 0) {
+        let earns = await minerContract.calculateMinersSell(
+          myMinersSinceLastHatch
+        );
+        earns = parseFloat(ethers.utils.formatEther(earns.toString())).toFixed(
+          4
+        );
+
+        if (earns < 0.000001) {
+          setMyEarns(0);
+        }
+        setMyEarns(earns);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (active && account && !minerContract) getContract();
+    if (minerContract) getContractStats();
+    const interval = setInterval(() => {
+      getContractStats();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [minerContract, account, active]);
 
   return (
     <>
-      <div className="conteiner-main-content">
-        <div className="main-section box">
-          <VStack width="100%">
-            <Box width="90%" display="flex" mt={50}>
-              <Text textAlign="left">Wallet ETH:</Text>
+      {isSmallerThan950 ? (
+        <VStack width="300px">
+          <HStack alignItems="flex-start" flexWrap="wrap">
+            <Box width="100%" mt={20}>
+              <MainSection
+                accountBalance={accountBalance}
+                miners={myMiners}
+                contract={minerContract}
+                myEarns={myEarns}
+              />
+            </Box>
 
-              <Text flex="1" textAlign="right">
-                0
-              </Text>
-            </Box>
-            <Box width="90%" display="flex" mt="25px !important">
-              <Text textAlign="left">Your miners:</Text>
-              <Text flex="1" textAlign="right">
-                0
-              </Text>
-            </Box>
-            <Box width="90%" display="flex" mt="25px !important">
-              <Text textAlign="left">Daily return up to:</Text>
-              <Text flex="1" textAlign="right">
-                ~10%
-              </Text>
-            </Box>
-            <Box width="90%" mt="25px !important">
-              <Input
-                background="gray.100"
-                type="number"
-                min="0"
-                placeholder="ETH"
-                textAlign="center"
-                onChange={(e) => setEthValue(e.target.value)}
-              ></Input>
-              <Button mt={15} width="100%">
-                Hire Farmers
-              </Button>
-            </Box>
-          </VStack>
-        </div>
-        <div className="reward-info-section">
-          <div className="rewards box">
-            <Rewards></Rewards>
-          </div>
-
-          <div className="info box"></div>
-        </div>
-        <div className="referal box">
-          <VStack width="100%" display="flex" justifyContent="center">
-            <Text textAlign="left">Referal Program</Text>
-            <HStack width="95%" mt="15px !important">
-              <Box
-                ml={10}
-                background="gray.100"
-                width="95%"
-                height="auto"
-                padding="9px 4px 9px 4px"
-                fontSize={14}
-                borderRadius="5px"
-                color="gray.400"
-                textTransform="none"
-              >
-                {REFLINK}
+            <VStack width="100%" marginInlineStart="0px !important">
+              <Box width="100%" mt={5}>
+                <Rewards
+                  contract={minerContract}
+                  reward={myEarns}
+                  accountBalance={accountBalance}
+                  miners={myMiners}
+                />
               </Box>
-              <Button>Copy</Button>
-            </HStack>
-            <Text fontSize={12} mt="17px !important">
-              Earn 10% of the Miners from anyone who uses your referral link
-              used to open the mine!
-            </Text>
-          </VStack>
-        </div>
-      </div>
+              <Box width="100%">
+                <InfoBox contractBalance={contractBalance} />
+              </Box>
+            </VStack>
+          </HStack>
+          <Box
+            width="100%"
+            display="flex"
+            justifyContent="center"
+            mt="20px !important"
+          >
+            <Referal account={account} />
+          </Box>
+        </VStack>
+      ) : (
+        <VStack width="900px" mt={55}>
+          <HStack alignItems="flex-start" gap={4}>
+            <Box width="100%">
+              <MainSection
+                accountBalance={accountBalance}
+                miners={myMiners}
+                contract={minerContract}
+                myEarns={myEarns}
+              />
+            </Box>
+
+            <VStack width="90%">
+              <Box width="100%" height="fit-content">
+                <Rewards
+                  contract={minerContract}
+                  reward={myEarns}
+                  accountBalance={accountBalance}
+                  miners={myMiners}
+                />
+              </Box>
+              <Box width="100%">
+                <InfoBox contractBalance={contractBalance} />
+              </Box>
+            </VStack>
+          </HStack>
+          <Box
+            width="100%"
+            display="flex"
+            justifyContent="center"
+            mt="20px !important"
+          >
+            <Referal account={account} />
+          </Box>
+        </VStack>
+      )}
     </>
   );
 }
